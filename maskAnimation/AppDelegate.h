28e41980dbb4528dba71f18967880e4b1dcdaab6
Axel Kee <axelshaded@gmail.com>
2017-09-12 13:03:03 +0800

    Initial Commit

---
+++ maskAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  maskAnimation
+//
+//  Created by Axel Kee on 12/09/2017.
+//  Copyright © 2017 Sweatshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
