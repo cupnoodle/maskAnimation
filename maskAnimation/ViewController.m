57429b0dd0a86e1ee0f034993d6a210b97ce3379
Axel Kee <axelshaded@gmail.com>
2017-09-12 14:19:48 +0800

    managed to get square mask working

--- maskAnimation/ViewController.m
+++ maskAnimation/ViewController.m
@@ -6,13 +6,18 @@
 //  Copyright © 2017 Sweatshop. All rights reserved.
 //
 
-#import "ViewController.h"
 
+#import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 @interface ViewController ()
+
+
 
 @end
 
 @implementation ViewController
+
+CAShapeLayer *maskLayer;
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
@@ -25,5 +30,34 @@
 	// Dispose of any resources that can be recreated.
 }
 
+- (IBAction)animateTapped:(id)sender {
+	[self animateMask:maskLayer forImageView:self.tailsImage];
+}
+
+- (void)animateMask:(CAShapeLayer *)mask  forImageView:(UIImageView *)image{
+	
+	// https://stackoverflow.com/questions/11391058/simply-mask-a-uiview-with-a-rectangle
+	
+	
+	if(image == nil){
+		return;
+	}
+	
+	mask = [[CAShapeLayer alloc] init];
+	
+	// Create a path with the rectangle in it.
+	CGRect maskRect = CGRectMake(0, 0, 100, 100);
+	CGPathRef path = CGPathCreateWithRect(maskRect, NULL);
+	
+	
+	mask.path = path;
+	CGPathRelease(path);
+	
+	image.layer.mask = mask;
+	image.layer.masksToBounds = YES;
+	
+
+	
+}
 
 @end
